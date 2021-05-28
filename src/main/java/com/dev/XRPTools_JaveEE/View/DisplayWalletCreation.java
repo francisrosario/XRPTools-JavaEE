5e8122233549fea5d31729cc910d48545249942d
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-05-28 08:37:00 +0800

    DisplayWalletCreation.java: Remove svg

--- src/main/java/com/dev/XRPTools_JaveEE/View/DisplayWalletCreation.java
+++ src/main/java/com/dev/XRPTools_JaveEE/View/DisplayWalletCreation.java
@@ -1330,8 +1330,7 @@ public class DisplayWalletCreation extends HttpServlet {
                 "\t<!--end::Javascript-->\n" +
                 "\n" +
                 "\n" +
-                "svg id=\"SvgjsSvg1001\" width=\"2\" height=\"0\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" style=\"overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;\"&gt;<defs id=\"SvgjsDefs1002\"></defs><polyline id=\"SvgjsPolyline1003\" points=\"0,0\"></polyline><path id=\"SvgjsPath1004\" d=\"M0 0 \"></path>\n" +
-                "<svg id=\"SvgjsSvg1001\" width=\"2\" height=\"0\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" style=\"overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;\"><defs id=\"SvgjsDefs1002\"></defs><polyline id=\"SvgjsPolyline1003\" points=\"0,0\"></polyline><path id=\"SvgjsPath1004\" d=\"M0 0 \"></path></svg></body><!--end::Body--></html>");
+                "</body><!--end::Body--></html>");
 
         }
     }
