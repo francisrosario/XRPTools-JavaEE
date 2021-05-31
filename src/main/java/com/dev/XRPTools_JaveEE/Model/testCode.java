87d41ff0723a3559ad79eb320034e08edec2e2db
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-05-31 12:03:40 +0800

    testCode: Add if condition to specific function for testing

--- src/main/java/com/dev/XRPTools_JaveEE/Model/testCode.java
+++ src/main/java/com/dev/XRPTools_JaveEE/Model/testCode.java
@@ -73,7 +73,15 @@ public class testCode {
         //
         //
 
+        //Simple Vanity address generator - Testnet
         boolean seatchforAddress = false;
+        //Fund Wallet - Testnet
+        int rounds = 6;
+        boolean doFunding = false;
+        //Check Wallet Transaction
+        boolean checkRAWTransaction = false;
+        //Check if Account is Activated or not
+        boolean checkAccActivated = false;
 
         //Vanity Address generator simple || Vanity Address Generator feature
         if(seatchforAddress) {
@@ -105,70 +113,71 @@ public class testCode {
         // Fund Wallet by 1000XRP per Iteration || Testnet Dev faucet feature
         XrplClient xrplClient = new XrplClient(HttpUrl.get("https://s.altnet.rippletest.net:51234/"));
         FaucetClient faucetClient = FaucetClient.construct(HttpUrl.get("https://faucet.altnet.rippletest.net"));
-        /**
-         for(int x = 0; x < 1; x++){
-         faucetClient.fundAccount(FundAccountRequest.of(wallet.classicAddress()));
-         }
-         **/
+
+        if(doFunding){
+            for(int x = 1; x < rounds; x++){
+                System.out.println("Funding Account counter: " + x);
+                faucetClient.fundAccount(FundAccountRequest.of(wallet.classicAddress()));
+            }
+        }
 
         AccountInfoResult accountInfoResult = xrplClient.accountInfo(AccountInfoRequestParams.of(wallet.classicAddress()));
         AccountObjectsResult or = xrplClient.accountObjects(AccountObjectsRequestParams.of(wallet.classicAddress()));
 
 
-        //Check if Wallet is Validated in Ledger || If Account is Validted in Ledger proceed to XRP Wallet Tools dashboard
-        System.out.println("Newly generated seed: " + seedResult.seed());
-
-
-
-        try {
-            AccountInfoRequestParams params = AccountInfoRequestParams.builder()
-                    .account(wallet.classicAddress())
-                    .ledgerIndex(LedgerIndex.VALIDATED)
-                    .build();
-            System.out.println("Account is Activated/Not : " + xrplClient.accountInfo(params).validated());
-        } catch (Exception e) {
-            if(e.getMessage().equals("Account not found.")){
-                //Go to error page
+        //Check if Wallet is Validated in Ledger || If Account is Validated in Ledger proceed to XRP Wallet Tools dashboard
+        if(checkAccActivated) {
+            System.out.println("Newly generated seed: " + seedResult.seed());
+            try {
+                AccountInfoRequestParams params = AccountInfoRequestParams.builder()
+                        .account(wallet.classicAddress())
+                        .ledgerIndex(LedgerIndex.VALIDATED)
+                        .build();
+                System.out.println("Account is Activated/Not : " + xrplClient.accountInfo(params).validated());
+            } catch (Exception e) {
+                if (e.getMessage().equals("Account not found.")) {
+                    //Go to error page
+                }
+                throw new RuntimeException(e.getMessage(), e);
             }
-            throw new RuntimeException(e.getMessage(), e);
         }
-
         //Check transactions by Ledger range || For Show Transaction feature
 
-        // known ledger index range for this account that is known to have exactly 748 transactions
-        LedgerIndex minLedger = LedgerIndex.of(UnsignedLong.valueOf(17896000));
-        String cLedgerIndex = String.valueOf(accountInfoResult.ledgerCurrentIndex());
-        UnsignedLong cLedgerIndexLong = UnsignedLong.valueOf(cLedgerIndex.substring(9, cLedgerIndex.length() - 1));
-        LedgerIndex maxLedger = LedgerIndex.of(UnsignedLong.valueOf(String.valueOf(cLedgerIndexLong)));
+        if(checkRAWTransaction) {
+            // known ledger index range for this account that is known to have exactly 748 transactions
+            LedgerIndex minLedger = LedgerIndex.of(UnsignedLong.valueOf(17896000));
+            String cLedgerIndex = String.valueOf(accountInfoResult.ledgerCurrentIndex());
+            UnsignedLong cLedgerIndexLong = UnsignedLong.valueOf(cLedgerIndex.substring(9, cLedgerIndex.length() - 1));
+            LedgerIndex maxLedger = LedgerIndex.of(UnsignedLong.valueOf(String.valueOf(cLedgerIndexLong)));
 
 
-        AccountTransactionsResult results = xrplClient.accountTransactions(AccountTransactionsRequestParams.builder()
-                .account(wallet.classicAddress())
-                .ledgerIndexMin(minLedger)
-                .ledgerIndexMax(maxLedger)
-                .build());
-
-        int transactionsFound = results.transactions().size();
-        while (results.marker().isPresent()) {
-            results = xrplClient.accountTransactions(AccountTransactionsRequestParams.builder()
+            AccountTransactionsResult results = xrplClient.accountTransactions(AccountTransactionsRequestParams.builder()
                     .account(wallet.classicAddress())
                     .ledgerIndexMin(minLedger)
                     .ledgerIndexMax(maxLedger)
-                    .marker(results.marker().get())
                     .build());
-            transactionsFound += results.transactions().size();
+
+            int transactionsFound = results.transactions().size();
+            while (results.marker().isPresent()) {
+                results = xrplClient.accountTransactions(AccountTransactionsRequestParams.builder()
+                        .account(wallet.classicAddress())
+                        .ledgerIndexMin(minLedger)
+                        .ledgerIndexMax(maxLedger)
+                        .marker(results.marker().get())
+                        .build());
+                transactionsFound += results.transactions().size();
+            }
+
+            System.out.println("Raw Transactions output :\n" + results.transactions());
+            System.out.println("Transaction Found : " + transactionsFound);
+            System.out.println("Wallet Classic Address: " + wallet.classicAddress());
         }
 
-        System.out.println("Raw Transactions output :\n" + results.transactions());
-        System.out.println("Transaction Found : " + transactionsFound);
-        System.out.println("Wallet Classic Address: " + wallet.classicAddress());
-
+        System.out.println("\n\nUsing HttpURLConnection for JSON Request");
         // Method #1 JSON req using HttpURLConnection
         URL endpoint = new URL ("https://s.altnet.rippletest.net:51234/");
         HttpURLConnection HttpUrl = (HttpURLConnection)endpoint.openConnection();
         HttpUrl.setRequestMethod("POST");
-        //con.setRequestProperty("Content-Type", "application/json; utf-8");
-        //con.setRequestProperty("Accept", "application/json");
         HttpUrl.setDoOutput(true);
 
         String jsonInputString = "{\n" +
@@ -203,19 +212,12 @@ public class testCode {
         // Method #2 using xrp4j lib.
 
         // Example i want to check rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn
-        /**
-         JsonRpcResponse<String>
-         JsonRpcClient jsonRpcClient = JsonRpcClient.construct(HttpUrl.get("https://s.altnet.rippletest.net:51234/"));
+         //JsonRpcClient jsonRpcClient = JsonRpcClient.construct(HttpUrl.get("https://s.altnet.rippletest.net:51234/"));
          //JavaType type = TypeFactory.defaultInstance().constructType(.class);
-         try{
          JsonRpcRequest request = JsonRpcRequest.builder()
          .method("ledger_closed")
          .build();
-         jsonRpcClient.send(request, c);
 
-         }catch (Exception e) {
-         throw new RuntimeException(e.getMessage(), e);
-         }
-         **/
+
     }
 }
