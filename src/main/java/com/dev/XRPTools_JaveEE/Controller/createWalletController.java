6216cfaf66d4c59e1bfeddbfe89cc5dad56a82c2
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-06-03 05:19:53 +0800

    DisplayWalletCreation.java: Moved CSS as External

--- src/main/java/com/dev/XRPTools_JaveEE/Controller/createWalletController.java
+++ src/main/java/com/dev/XRPTools_JaveEE/Controller/createWalletController.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.dev.XRPTools_JaveEE.Model.createXRPWallet;
 import org.xrpl.xrpl4j.client.JsonRpcClientErrorException;
 
