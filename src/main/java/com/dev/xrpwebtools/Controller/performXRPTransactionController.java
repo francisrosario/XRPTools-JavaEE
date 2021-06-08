d5c9a9c3da4ad6328e6345d01c0e7feeaeb17804
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-06-09 07:24:02 +0800

    performXRPTransactionController.java: Prepare controller for sending XRP.

--- src/main/java/com/dev/xrpwebtools/Controller/performXRPTransactionController.java
+++ src/main/java/com/dev/xrpwebtools/Controller/performXRPTransactionController.java
@@ -1,4 +1,35 @@
 package com.dev.xrpwebtools.Controller;
 
-public class performXRPTransactionController {
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dev.xrpwebtools.Model.XRPConn;
+
+public class performXRPTransactionController extends HttpServlet{
+    //////////////////////
+    //Utils
+    private final Logger logger = Logger.getLogger(dashboardController.class.getName());
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        try {
+            String walletseed = req.getParameter("walletseed");
+            RequestDispatcher dispatcher = req.getRequestDispatcher("");
+            dispatcher.forward(req, resp);
+        } catch (Exception e) {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("error.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
+    @Override
+    public void destroy() {
+        logger.info("servlet taken out of service.");
+    }
+
 }
